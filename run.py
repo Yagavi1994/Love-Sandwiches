e30338b58fd19b7088bf54e4a6af2efe13141aef
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-04-23 09:12:21 +0000

    Add udate_sales_data function to update the sales data in sales worksheet.

--- run.py
+++ run.py
@@ -12,18 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -32,9 +39,7 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-
     try:
-
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
@@ -42,6 +47,24 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+def update_sales_data():
+    """
+    Update the user enetred value in the sales worksheet in gspread
+    """
+
+    print("Updating sales data...\n")
+    sheets_worksheet = SHEET.worksheet("sales")
+    sheets_worksheet.append_row(data)
+    print("Sales data updated successfully.\n")
 
 
-get_sales_data()
+
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_data()

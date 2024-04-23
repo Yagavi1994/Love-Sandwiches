4021dea5e9e47e2b225d69947b5286f8acf8cbc1
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-04-23 17:13:51 +0000

    Add get last 5 entries from sales funtion.

--- run.py
+++ run.py
@@ -86,6 +86,20 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet successfully updated.\n")
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
@@ -100,4 +114,7 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation\n")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
+print(sales_columns)
